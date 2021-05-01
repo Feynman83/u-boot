16ad6385c43df0235ee0e03f994658961843bed8
Feynman <codingmonkey83@gmail.com>
2021-05-01 15:18:55 +0800

    add uboot env board detect

--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -41,7 +41,7 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"image=zImage\0" \
-	"fdtfile=imx6q-rbc-phx2.dtb\0" \
+	"fdtfile=undefined\0" \
 	"fdt_addr=0x18000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
@@ -120,18 +120,10 @@
 		"fi;\0" \
 		"findfdt="\
 			"if test $fdtfile = undefined; then " \
-				"if test $board_name = SABREAUTO && test $board_rev = MX6QP; then " \
-					"setenv fdtfile imx6qp-sabreauto.dtb; fi; " \
-				"if test $board_name = SABREAUTO && test $board_rev = MX6Q; then " \
-					"setenv fdtfile imx6q-sabreauto.dtb; fi; " \
-				"if test $board_name = SABREAUTO && test $board_rev = MX6DL; then " \
-					"setenv fdtfile imx6dl-sabreauto.dtb; fi; " \
-				"if test $board_name = SABRESD && test $board_rev = MX6QP; then " \
-					"setenv fdtfile imx6qp-sabresd.dtb; fi; " \
-				"if test $board_name = SABRESD && test $board_rev = MX6Q; then " \
-					"setenv fdtfile imx6q-sabresd.dtb; fi; " \
-				"if test $board_name = SABRESD && test $board_rev = MX6DL; then " \
-					"setenv fdtfile imx6dl-sabresd.dtb; fi; " \
+				"if test gpio input 192 = 0 ; then " \
+					"setenv fdtfile imx6q-rbc-rj45.dtb; fi; " \
+				"if test gpio input 192 = 1; then " \
+					"setenv fdtfile imx6q-rbc-phx2.dtb; fi; " \
 				"if test $fdtfile = undefined; then " \
 					"echo WARNING: Could not determine dtb to use; fi; " \
 			"fi;\0" \
